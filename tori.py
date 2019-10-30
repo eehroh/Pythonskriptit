079b6d3ed86dfb8705fe26db7144186cb4f9f030
eehroh <eeroro@protonmail.com>
2019-10-30 20:05:19 +0200

    torin uudistus

--- tori.py
+++ tori.py
@@ -1,7 +1,8 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import csv
 
-my_url = "https://www.tori.fi/koko_suomi/vaatteet_ja_kengat?ca=18&q=adidas%20boost&cg=3050&st=s&cs=2&w=3&ck=16"
+my_url = "https://www.tori.fi/koko_suomi?q=adidas&cg=3050&w=3&st=s&cs=2&ck=16&csz=134&ca=18&l=0&md=th"
 
 uClient = uReq(my_url)
 page_html = uClient.read()
@@ -11,7 +12,7 @@ page_soup = soup(page_html, "html.parser")
 
 containers = page_soup.findAll("div", {"class":"desc"})
 
-filename = "torikengät_boost.csv"
+filename = "torikengät.csv"
 f = open(filename, "w")
 
 headers = "KENKÄ, HINTA\n"
@@ -19,14 +20,14 @@ headers = "KENKÄ, HINTA\n"
 f.write(headers)
 
 for container in containers:
-    kenkä = container.a.text
+    kenka_container = container.findAll("div", {"class":"li-title"})
+    kenka = kenka_container[0].text
     
     hinta_container = container.findAll("p", {"class":"list_price"})
     hinta = hinta_container[0].text
 
 
-    f.write(kenkä.replace(",", "/") + "," + hinta + "\n")
-
-    print("Valmis")
+    f.write(kenka.replace(",", "/") + "," + hinta + "\n")
 
 f.close
+print("Valmis")
